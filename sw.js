a0c6e9bdadc3d28ce8185904218fe40bbb45e640
tomsimmer <simmer.tom1@gmail.com>
2026-09-06 20:59:10 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'hausplan-v3';
+const CACHE = 'hausplan-v4';
 const ASSETS = ['./','./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
 
 self.addEventListener('install', e => {
